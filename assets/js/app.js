7006c4375f18f825d32637b7e8b7b0ae46006637
KarinAAC <karin_alejo@hotmail.com>
2017-06-02 17:28:49 -0500

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+var slider = $('#slider');
+var prev= $('#btn-prev');
+var sig= $('#btn-next');
+
+$('#slider section:last').insertBefore('#slider section:first');
+slider.css('margin-left', '-'+100+'%')
+
+function moverSiguiente() {
+    slider.animate({marginleft:''+200+'%'},700,function(){
+      $('#slider section:first').insertAfter('#slider section:last');
+      slider.css('margin-left', '-'+100+'%')
+
+  });
+}
+sig.on('click', function () {
+  moverSiguiente();
+});
+
+function moverPrevio() {
+    slider.animate({marginleft:0},700,function(){
+      $('#slider section:last').insertBefore('#slider section:first');
+      slider.css('margin-left', '-'+100+'%')
+
+  });
+}
+prev.on('click', function () {
+  moverPrevio();
+});
